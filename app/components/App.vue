8b2fc23a90974343cec99dca5b30d3d173c06b5f
Emil Sallem <emhesa@gmx.de>
2018-10-22 17:53:32 +0100

    git add .

---
+++ app/components/App.vue
@@ -0,0 +1,45 @@
+
+<template>
+    <Page>
+        <ActionBar title="nearBuy"/>
+        <GridLayout columns="*" rows="*">
+            <Label class="message" :text="msg" col="0" row="0"/>
+            <Button text="Next" @tap="onButtonTap" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    //Importing the map component
+    import Map from './map'
+    export default {
+        methods: {
+            onButtonTap() {
+            this.$navigateTo(Map);
+        }
+    },
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+
+
+
+
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ app/components/map.vue
@@ -0,0 +1,48 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar" title="nearBuy"></ActionBar>
+        <GridLayout>
+                <Mapbox
+                    accessToken="pk.eyJ1IjoiZW1pbHNhbGxlbSIsImEiOiJjam5rZ3BibnMwZjZmM3dwazhsM29pcWg2In0.Kn7kBXDI7niAIHvwS2iDMw"
+                    mapStyle="traffic_day"
+                    latitude="37.7397"
+                    longitude="-121.4252"
+                    hideCompass="true"
+                    zoomLevel="12"
+                    showUserLocation="false"
+                    disableZoom="false"
+                    disableRotation="false"
+                    disableScroll="false"
+                    disableTilt="false"
+                    @mapReady="onMapReady($event)">
+                </Mapbox>
+        </GridLayout>
+    </Page>
+</template>
+
+
+<script>
+    import * as utils from "utils/utils";
+    export default {
+        data () {
+            return { };
+        },
+        methods: {
+            onMapReady(args) {
+                args.map.addMarkers([
+                    {
+                        lat: 37.7397,
+                        lng: -121.4252,
+                        title: "Tracy, CA",
+                        subtitle: "Home of The Polyglot Developer!",
+                        onCalloutTap: () => {
+                            utils.openUrl("https://www.thepolyglotdeveloper.com");
+                        }
+                    }
+                ]);
+            }
+        }
+    };
+</script> 
+ 
+
